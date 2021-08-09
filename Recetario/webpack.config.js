7952140927c70e472f3825143031da6e0c871e0e
Tapia <zs17012957@estudiantes.uv.mx>
2021-08-09 13:18:05 -0500

    Se crea y configura archivo webpackconfing

---
+++ Recetario/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'recetario_dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
+
